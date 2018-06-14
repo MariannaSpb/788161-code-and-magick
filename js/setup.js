f299d9b90602d4d412f99da4a7da5ce02cbdef50
Маря <marry-cherry-pie@mail.ru>
2018-06-15 02:09:36 +0300

    открытие\закрытие окна+ валидация формы

--- js/setup.js
+++ js/setup.js
@@ -1,13 +1,18 @@
 'use strict';
 // откываем окно настройки:
-var setupWindow = document.querySelector('.setup');
-setupWindow.classList.remove('hidden');
+// var setupWindow = document.querySelector('.setup');
+// setupWindow.classList.remove('hidden');
+
+var setupOpen = document.querySelector('.setup-open');
+var setup = document.querySelector('.setup');
 
 // создаю 4 массива данных о персонажах
 var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 
 // функция случайного сочетания имя + фамилия
 var renderName = function () {
@@ -54,3 +59,55 @@ similarListElement.appendChild(fragment);
 document.querySelector('.setup-similar').classList.remove('hidden');
 
 
+// Нажатие на элемент .setup-close, расположенный внутри блока setup возвращает ему класс hidden.
+var setupClose = setup.querySelector('.setup-close');
+
+// обработчики для альтернативного ввода с клавиатуры keydown или point on click
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+// Валидация ввода имени персонажа
+var userNameInput = setup.querySelector('.setup-user-name');
+userNameInput.addEventListener('invalid', function (evt) {
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity('имя персонажа не может содержать менее 2 символов');
+  } else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity('максимальная длина имени персонажа — 25 символов');
+  } else if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
