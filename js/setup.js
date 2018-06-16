e135baff369301efd5d79617c2cf4185f1c67489
Маря <marry-cherry-pie@mail.ru>
2018-06-17 00:30:23 +0300

    окно настройки персонажа

--- js/setup.js
+++ js/setup.js
@@ -3,14 +3,13 @@
 // var setupWindow = document.querySelector('.setup');
 // setupWindow.classList.remove('hidden');
 
-var setupOpen = document.querySelector('.setup-open');
-var setup = document.querySelector('.setup');
 
-// создаю 4 массива данных о персонажах
+// создаю массивы данных о персонажах
 var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 var ESC_KEYCODE = 27;
 var ENTER_KEYCODE = 13;
 
@@ -33,7 +32,15 @@ var renderEyes = function () {
   return EYES_COLORS[randomEyesColors];
 };
 
-// нашлина странице список волшебников:
+
+// функция случайного цвета файрбола
+var renderFireball = function () {
+  var randomFireballColors = Math.floor(Math.random() * FIREBALL_COLORS.length);
+  return FIREBALL_COLORS[randomFireballColors];
+};
+
+
+// нашли на странице список волшебников:
 var similarListElement = document.querySelector('.setup-similar-list');
 
 // нахожу шаблон на странице с помощью querySelectot  по id и находим div который описывает волшебника:
@@ -58,56 +65,101 @@ similarListElement.appendChild(fragment);
 // открываем блок для  похожих героев
 document.querySelector('.setup-similar').classList.remove('hidden');
 
+// Одеть Надежду
+// tabindex="0">
 
-// Нажатие на элемент .setup-close, расположенный внутри блока setup возвращает ему класс hidden.
+// Нажатие на элемент .setup-open удаляет класс hidden  у блока setup
+var setupOpen = document.querySelector('.setup-open');
+var setup = document.querySelector('.setup');
 var setupClose = setup.querySelector('.setup-close');
+var userNameInput = document.querySelector('.setup-user-name');
 
-// обработчики для альтернативного ввода с клавиатуры keydown или point on click
-var onPopupEscPress = function (evt) {
+var dialogEscPress = function (evt) {
   if (evt.keyCode === ESC_KEYCODE) {
-    closePopup();
+    closeDialog();
   }
 };
 
-var openPopup = function () {
+
+// функции открытия и закрытия окна
+var openDialog = function () {
   setup.classList.remove('hidden');
-  document.addEventListener('keydown', onPopupEscPress);
+  document.addEventListener('keydown', dialogEscPress);
 };
 
-var closePopup = function () {
+var closeDialog = function () {
+  // if (!valueMissing) {
+  //   return;
+  // }
   setup.classList.add('hidden');
-  document.removeEventListener('keydown', onPopupEscPress);
+  document.removeEventListener('keydown', dialogEscPress);
 };
 
+// Окно.setup должно открываться по нажатию на блок.setup-open.
 setupOpen.addEventListener('click', function () {
-  openPopup();
+  openDialog();
 });
 
+// Когда иконка пользователя в фокусе, то окно  должно открываться по нажатию кнопки ENTER
 setupOpen.addEventListener('keydown', function (evt) {
   if (evt.keyCode === ENTER_KEYCODE) {
-    openPopup();
+    openDialog();
   }
 });
-
+// Окно.setup должно закрываться по нажатию на элемент.setup-close, расположенный внутри окна (возврат класса hidden)
 setupClose.addEventListener('click', function () {
-  closePopup();
+  closeDialog();
 });
-
+// Если окно открыто и фокус находится на кнопке закрытия окна, то нажатие клавиши ENTER должно приводить к закрытию диалога
 setupClose.addEventListener('keydown', function (evt) {
   if (evt.keyCode === ENTER_KEYCODE) {
-    closePopup();
+    closeDialog();
   }
 });
-// Валидация ввода имени персонажа
-var userNameInput = setup.querySelector('.setup-user-name');
-userNameInput.addEventListener('invalid', function (evt) {
+
+
+// валидация формы
+
+userNameInput.addEventListener('invalid', function () {
   if (userNameInput.validity.tooShort) {
-    userNameInput.setCustomValidity('имя персонажа не может содержать менее 2 символов');
+    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
   } else if (userNameInput.validity.tooLong) {
-    userNameInput.setCustomValidity('максимальная длина имени персонажа — 25 символов');
+    userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
   } else if (userNameInput.validity.valueMissing) {
     userNameInput.setCustomValidity('Обязательное поле');
   } else {
     userNameInput.setCustomValidity('');
   }
 });
+
+var setupWizard = document.querySelector('.setup-wizard');
+var wizardCoat = setupWizard.querySelector('.wizard-coat');
+var wizardEyes = setupWizard.querySelector('.wizard-eyes');
+var wizardFireball = document.querySelector('.setup-wizard-wrap');
+var inputChangeCoat = document.querySelector('input[name=coat-color]');
+var inputChangeEyes = document.querySelector('input[name=eyes-color]');
+var inputChangeFireball = document.querySelector('input[name=fireball-color]');
+
+// Изменение цвета мантии персонажа по нажатию. Цвет мантии .setup-wizard .wizard-coat должен обновляться по нажатию на неё.
+wizardCoat.addEventListener('click', function () {
+  var color = renderCoat();
+  wizardCoat.style.fill = color;
+  inputChangeCoat.value = color;
+});
+
+// Изменение цвета глаз персонажа по нажатию.
+wizardEyes.addEventListener('click', function () {
+  var color = renderEyes();
+  wizardEyes.style.fill = color;
+  inputChangeEyes.value = color;
+});
+
+
+// Изменение цвета фаерболов по нажатию
+wizardFireball.addEventListener('click', function () {
+  var color = renderFireball();
+  wizardFireball.style.background = color;
+  inputChangeFireball.value = color;
+});
+
+
